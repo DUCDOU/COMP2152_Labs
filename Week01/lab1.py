abd58f8f30e1d4329047cc2b22b501e89744abc4
DUC DOU <gianghbh192@gmail.com>
2026-01-27 12:24:33 -0500

    lab3

---
+++ Week01/lab1.py
@@ -0,0 +1,34 @@
+# Simple Coding Question 01 Week 01
+# Gia Duc Can
+
+# Question 2: Defining an array (list)
+cart = [1, 4, 7, 9]
+
+# Question 3: Order Of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Expression:
+# e = a - b ** c // d + a % c
+
+# Bracket version
+e = (a - ((b ** c) // d)) + (a % c)
+#e = (1 - ((2^3) // 4)) + (1 % 3)
+#e = (1 - (8 // 4) + 1
+#e = (1 - 2) + 1
+#e = -1 + 1
+#e = 0
+print("question 3 result:", e = 0)
+
+#Question 4: Formatting
+temprature = 32.6
+print("the temprature today is: {:.3f} degrees celsius".format(temprature))
+
+#Question 5: Common functions - User input
+userAge = int(input("Enter your age:"))
+userAge = userAge + 22
+print("now showing the shop items filtered by age:", userAge)
+
+
