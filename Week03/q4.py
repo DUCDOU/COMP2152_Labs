ff5eb83432da1ec1190d42c77736a58f7ed3c963
DUC DOU <gianghbh192@gmail.com>
2026-02-03 12:24:47 -0500

    labweek4

--- Week03/q4.py
+++ Week03/q4.py
@@ -1,18 +1,28 @@
-monday_class = {"Alice", "Bob", "Charlie", "Diana"}
 
-wednesday_class = {"Bob", "Diana", "Eve", "Frank",}
+monday_class = {"Alice", "Bob", "Charlie", "Diana"}
+wednesday_class = {"Bob", "Diana", "Eve", "Frank"}
+
 
 monday_class.add("Grace")
-print("Monday Class"), monday_class
-print("Wednesday Class", wednesday_class)
+
+print("Monday class:", monday_class)
+print("Wednesday class:", wednesday_class)
+
 
 both_classes = monday_class & wednesday_class
-print("attended both classes:" ,both_classes)
+print("Attended both classes:", both_classes)
+
 
 all_students = monday_class | wednesday_class
-print("Attended either classes", all_students)
+print("Attended either class:", all_students)
+
+
 only_monday = monday_class - wednesday_class
-print("Only Monday: ", only_monday)
-only_one - monday_class ^ wednesday_class
-print("Only one class:", only_one)
+print("Only Monday:", only_monday)
+
+
+only_one = monday_class ^ wednesday_class
+print("Only one class (not both):", only_one)
+
+
 print("Is Monday subset of all students?", monday_class <= all_students)
